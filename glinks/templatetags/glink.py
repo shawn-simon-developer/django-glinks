f9eb8bb076255e6c466f6b84d7e0fd44c13cb78b
Shawn Simon <shawn.simon.developer@gmail.com>
2014-06-12 13:52:46 -0400

    Adding custom glinks + standard glinks

--- glinks/templatetags/glink.py
+++ glinks/templatetags/glink.py
@@ -22,7 +22,7 @@ def parseGlinkNode(command):
 			options["as"] = command[command.index("as")+1]
 	return options
 
-class GlinkNode(Node):
+class GlinkNodeCustom(Node):
 	child_nodelists = ('nodelist_file', 'nodelist_empty')
 
 	def __init__(self, parser, token):
@@ -43,9 +43,9 @@ class GlinkNode(Node):
 		# Close block.
 		if command[-2] == 'as':
 			self.as_var = command[-1]
-			self.nodelist_file = parser.parse(('empty', 'endglink',))
+			self.nodelist_file = parser.parse(('empty', 'endcustom_glink',))
 			if parser.next_token().contents == 'empty':
-				self.nodelist_empty = parser.parse(('endglink',))
+				self.nodelist_empty = parser.parse(('endcustom_glink',))
 				parser.delete_first_token()
 		
 		# Nifty little switcharoo from string to object.
@@ -74,18 +74,59 @@ class GlinkNode(Node):
 
 		return output
 
+class GlinkNode(Node):
+	child_nodelists = ('nodelist_file', 'nodelist_empty')
+
+	def __init__(self, parser, token):
+		command = token.split_contents()
+		self.must_be_first = None
+		self.as_var = None
+		self.blueprint = None
+		self.glink = None
+		self.options = {}
+		self.nodelist_file = None
+
+		self.options = parseGlinkNode(command)
+
+		for key, value in self.options.items():
+			if key == "blueprint":
+				self.blueprint = value
+		# Nifty little switcharoo from string to object.
+		self.blueprint = Blueprint.objects.get(name=self.blueprint)
+
+		try:
+			glinks = Glink.objects.filter(blueprint=self.blueprint)
+			self.glink = glinks.order_by('?')[0]
+
+			# Increment impression count.
+			self.glink.impressions = self.glink.impressions + 1
+			self.glink.save()
+		except Exception as e:
+			print str(e)
+
+	def render(self, context):
+		glink_page = "'glink/" + str(self.glink.pk) + "'"
+		img_lead = "<img src="
+		img_url = "'" + str(self.glink.image.url) + "'"
+		on_click = " onclick=location.href="
+		glink_page = "'/glink/" + str(self.glink.pk) + "'"
+		img_close = ">"
+ 		print img_lead + img_url + on_click + glink_page + img_close
+		return img_lead + img_url + on_click + glink_page + img_close
+
+
 @register.assignment_tag
 def getRandomGlink():
 	return Glink.objects.all().order_by('?')[0]
 		
 @register.tag
+def custom_glink(parser, token):
+	node = GlinkNodeCustom(parser, token)
+	return node
+
+@register.tag
 def glink(parser, token):
 	node = GlinkNode(parser, token)
 	return node
-
-@register.simple_tag
-def clickCounter():
-	glinkClickCounterForm = "<form> <input type='hidden' id='glinkClickCounter' name='glinkClickCounter'><br></form>"
-	return glinkClickCounterForm
 
 
